b1dd7b0767b4a4599ad63644ebe353c817fddc39
Rashiq <rashiq.z@gmail.com>
2014-08-01 05:54:40 +0200

    Add toggle for the spell checker

--- spellcheck.py
+++ spellcheck.py
@@ -1,8 +1,13 @@
 import sublime
 import sublime_plugin
 
+SETTINGS_FILE = 'Preferences.sublime-settings'
+SETTING_NAME = 'spell_check'
 
 class ToggleSpellCheckCommand(sublime_plugin.ApplicationCommand):
 
     def run(self):
-        pass
+        s = sublime.load_settings(SETTINGS_FILE)
+        spell_check_setting = s.get(SETTING_NAME, False)
+        s.set(SETTING_NAME, not spell_check_setting)
+        sublime.save_settigs(SETTINGS_FILE)
